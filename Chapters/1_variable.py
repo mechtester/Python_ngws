46f6e9a04b95152dbe41c442e504bbc94e8ab249
mechtester <kumarmechcad@gmail.com>
2022-04-23 12:28:38 +0500

    variables updated

---
+++ Chapters/1_variable.py
@@ -0,0 +1,12 @@
+# variable=value
+
+# sinle variable = single value
+name="vignesh"
+print(name)
+
+# multiple variable = multiple value
+name_1,name_2,name_3='vignesh','kumar','india'
+print(name_1,name_2,name_3)
+print(name_1)
+print(name_2)
+print(name_3)
