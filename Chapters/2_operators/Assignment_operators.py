34c6a615348bdc15b01c2244116b94b34326343f
mechtester <kumarmechcad@gmail.com>
2022-04-23 14:49:34 +0500

    Assignment operators created

---
+++ Chapters/2_operators/Assignment_operators.py
@@ -0,0 +1,78 @@
+#Total 13 Assignment operators
+# 1)Assign operator
+# 2)Add and Assign
+# 3)Subtract and Assign
+# 4)Multiply and Assign
+# 5)Divide and Assign
+# 6)Modulus and Assign
+# 7)Divide(floor) and Assign
+# 8)Exponent and Assign
+# 9)Bitwise and Assign
+# 10)Bitwise OR and Assign
+# 11)Bitwise XOR and Assign
+# 12)Bitwise Right shift and Assign
+# 13)Bitwise Left shift and Assign
+
+a=5
+b=2
+
+# 1)Assign operator
+x=a+b
+print("Assign operator",x)
+
+#2)Add and Assign
+#   +=
+a += b
+#a=a+b
+print("Add and Assign",a)
+
+#3)Subtract and Assign
+# -=
+a -= b
+#a=a-b
+print("Subtract and Assign",a)
+
+#4)Multiply and Assign
+# *=
+a *= b
+# a=a*b
+print("Multiply and Assign",a)
+
+# 5)Divide and Assign
+# /=
+a /= b
+# a=a/b
+print("Divide and Assign",a)
+
+# 6)Modulus and Assign
+# %=
+a %= b
+# a=a%b
+print("Modulus and Assign",a)
+
+# 7)Divide(floor) and Assign
+# //=
+a //= b
+# a=a//b
+print("Divide(floor) and Assign",a)
+
+# 8)Exponent and Assign
+# **=
+a **= b
+# a=a**b
+print("Exponent and Assign",a)
+
+# 9)Bitwise and Assign
+# &=
+a &= b
+# a=a&b
+print("Bitwise and Assign",a)
+
+# 10)Bitwise OR and Assign
+# |=
+a |= b
+# a=a|b
+print("Bitwise OR and Assign",a)
+
+# 11)Bitwise XOR and Assign
+#
---
+++ Chapters/2_operators/Operators.py
@@ -0,0 +1,23 @@
+# Be-sides assigning a variable an initial value, we can also perform the usual mathematical
+# operations on variables.
+
+# Basic Operators
+#  addition :  +
+#  subtraction :  -
+#  multiplication : *
+#  division : /
+#  floor division :  //  (rounds down the answer to the nearest whole number)
+#  modulus :  %  (gives the remainder when 5 is divided by 2)
+#  exponent : **  (5 to the power of 2)
+
+x=5
+y=2
+
+print("Additon:",x+y)
+print("Subtraction:",x-y)
+print("multiplication:",x*y)
+print("divison:",x/y)
+print("Floor division:",x//y)
+print("Modulus:",x%y)
+print("Eponent:",x**y)
+
